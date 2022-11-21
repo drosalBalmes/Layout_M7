bfeebe9a248056f0d6edff3f930d1a90930b5ca2
david <drosal@jaumebalmes.net>
2022-11-21 20:54:51 +0100

    Afegit radio y checked buttons

---
+++ app/src/main/java/com/example/layoutm7/RadioCheck.java
@@ -0,0 +1,65 @@
+package com.example.layoutm7;
+
+import android.os.Handler;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadioCheck extends AppCompatActivity {
+    CheckBox chkbox;
+    CheckBox chkbox2;
+    RadioButton rad1;
+    RadioButton rad2;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_check);
+        chkbox = findViewById(R.id.chkbx);
+        chkbox2 = findViewById(R.id.chkbx2);
+        rad1 = findViewById(R.id.rad1);
+        rad2 = findViewById(R.id.rad2);
+        text = findViewById(R.id.textView);
+
+
+        chkbox.setOnClickListener(arg0 -> {
+            if (chkbox.isChecked()) {
+                chkbox.setText("aceptas que te robemos todos los datos y se los vendamos al estado islamico?  !MuyBien!");
+            } else {
+                chkbox.setText("aceptas que te robemos todos los datos y se los vendamos al estado islamico?  Lo tendremos en cuenta....");
+            }
+        });
+
+        chkbox2.setOnClickListener(arg0 -> {
+            if (chkbox2.isChecked()) {
+                chkbox2.setText("Bueno algo hace");
+            }
+        });
+
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.rad1:
+                if (checked)
+                    text.setText("Opcio seleccionada: opcio 1");
+                    break;
+            case R.id.rad2:
+                if (checked)
+                    text.setText("Opcio seleccionada: opcio 2");
+
+                break;
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/layoutm7/twoLayouts.java
+++ app/src/main/java/com/example/layoutm7/twoLayouts.java
@@ -1,10 +1,8 @@
 package com.example.layoutm7;
 
+import android.content.Intent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.RadioGroup;
-import android.widget.ToggleButton;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,6 +14,8 @@ public class twoLayouts extends AppCompatActivity {
         setContentView(R.layout.activity_two_layouts);
         ToggleButton tb = (ToggleButton) findViewById(R.id.btnToggle);
         boolean isChecked;
+        ImageButton btn = findViewById(R.id.arrowbtn);
+        btn.setOnClickListener(view -> startActivity(new Intent(twoLayouts.this, RadioCheck.class)));
         tb.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View arg0) {
