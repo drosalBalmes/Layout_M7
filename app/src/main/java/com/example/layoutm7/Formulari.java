17dd5981babb3555a67c39f3b9b38653e536351c
david <drosal@jaumebalmes.net>
2022-12-01 17:44:52 +0100

    Spinners acabats

--- app/src/main/java/com/example/layoutm7/Formulari.java
+++ app/src/main/java/com/example/layoutm7/Formulari.java
@@ -11,6 +11,10 @@ import android.os.Bundle;
 
 public class Formulari extends AppCompatActivity {
     EditText nom;
+    EditText cognom;
+    EditText mail;
+    EditText telefon;
+    EditText adreça;
     Spinner spn;
 
     SharedPreferences sharedPreferences;
@@ -30,7 +34,11 @@ public class Formulari extends AppCompatActivity {
 //        spanString.setSpan(new StyleSpan(Typeface.BOLD), 0, spanString.length(), 0);
 //        spanString.setSpan(new StyleSpan(Typeface.ITALIC), 0, spanString.length(), 0);
 //        nom.setText(spanString);
-
+        nom = findViewById(R.id.nom);
+        cognom = findViewById(R.id.cognom);
+        mail = findViewById(R.id.mail);
+        telefon = findViewById(R.id.telefon);
+        adreça = findViewById(R.id.adreça);
         spn = (Spinner) (findViewById(R.id.spinner));
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.dgen, android.R.layout.simple_spinner_item);
@@ -54,7 +62,17 @@ public class Formulari extends AppCompatActivity {
         sharedPreferences = getSharedPreferences(
                 "Perfil", Context.MODE_PRIVATE);
         String spin = (String) spn.getSelectedItem();
+        String nom = this.nom.getText().toString();
+        String cognom = this.cognom.getText().toString();
+        String mail = this.mail.getText().toString();
+        String telefon = this.telefon.getText().toString();
+        String adreça = this.adreça.getText().toString();
         SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("nom",nom);
+        editor.putString("cognom",cognom);
+        editor.putString("mail",mail);
+        editor.putString("telefon",telefon);
+        editor.putString("adreça",adreça);
         editor.putString("genere", spin);
         editor.apply();
 
@@ -63,6 +81,16 @@ public class Formulari extends AppCompatActivity {
         sharedPreferences = getSharedPreferences(
                 "Perfil", Context.MODE_PRIVATE);
         String genere = sharedPreferences.getString("genere", "");
+        String nom = sharedPreferences.getString("nom","");
+        String cognom = sharedPreferences.getString("cognom","");
+        String mail = sharedPreferences.getString("mail","");
+        String telefon = sharedPreferences.getString("telefon","");
+        String adreça = sharedPreferences.getString("adreça","");
+        this.nom.setText(sharedPreferences.getString("nom",nom));
+        this.cognom.setText(sharedPreferences.getString("cognom",cognom));
+        this.mail.setText(sharedPreferences.getString("mail",mail));
+        this.telefon.setText(sharedPreferences.getString("telefon",telefon));
+        this.adreça.setText(sharedPreferences.getString("adreça",adreça));
         if (sharedPreferences.getString("genere", genere).equals("Masculí")) {
             spn.setSelection(0);
         }
