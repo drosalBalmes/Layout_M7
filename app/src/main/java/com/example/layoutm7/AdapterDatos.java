9a7cd278677b1026f1ccb7ed53390260cb250253
david <drosal@jaumebalmes.net>
2022-12-13 20:01:38 +0100

    Al clicar s'esborra + decoration + animator

--- app/src/main/java/com/example/layoutm7/AdapterDatos.java
+++ app/src/main/java/com/example/layoutm7/AdapterDatos.java
@@ -3,6 +3,8 @@ package com.example.layoutm7;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,9 +13,10 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
+public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> implements View.OnClickListener {
 
     ArrayList<String> listDatos;
+    private View.OnClickListener listener;
 
     public AdapterDatos(ArrayList<String> listDatos) {
         this.listDatos = listDatos;
@@ -24,17 +27,32 @@ public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDa
     @Override
     public AdapterDatos.ViewHolderDatos onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.auxiliar,null,false);
+        view.setOnClickListener(this);
         return new ViewHolderDatos(view);
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull AdapterDatos.ViewHolderDatos holder, int position) {
         holder.asignarDatos(listDatos.get(position));
+        Animation animation = AnimationUtils.loadAnimation(holder.itemView.getContext(), android.R.anim.slide_in_left);
+        holder.itemView.startAnimation(animation);
     }
 
     @Override
     public int getItemCount() {
         return listDatos.size();
+    }
+
+    public void setOnClickListener(View.OnClickListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (listener!=null){
+            listener.onClick(view);
+        }
     }
 
     public class ViewHolderDatos extends RecyclerView.ViewHolder {
--- app/src/main/java/com/example/layoutm7/RecyclerViewActivity.java
+++ app/src/main/java/com/example/layoutm7/RecyclerViewActivity.java
@@ -1,30 +1,69 @@
 package com.example.layoutm7;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.*;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
     RecyclerView recycler;
     ArrayList<String> listDatos;
+    Button dltbtn;
+    SwipeRefreshLayout swipeRefreshLayout;
+    AdapterDatos adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+        dltbtn = findViewById(R.id.dltbtn);
         recycler = findViewById(R.id.recyclerId);
+        swipeRefreshLayout = findViewById(R.id.swipe);
 //        recycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
         recycler.setLayoutManager(new GridLayoutManager(this,2));
         listDatos = new ArrayList<String>();
-        for (int i = 0; i < 100; i++) {
-            listDatos.add("Resultat agradable #" + i+ " ");
+        for (int i = 1; i < 100; i++) {
+            listDatos.add("Resultat agradable #" + i + " ");
 
         }
-        AdapterDatos adapter = new AdapterDatos(listDatos);
+        adapter = new AdapterDatos(listDatos);
+        recycler.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        recycler.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.HORIZONTAL));
+        recycler.setItemAnimator(new DefaultItemAnimator());
+
+
+        swipe();
+        adapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alClick(view);
+            }
+        });
         recycler.setAdapter(adapter);
+    }
+
+    public void swipe() {
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            swipeRefreshLayout.setRefreshing(false);
+        });
+
+    }
+
+    public void alClick(View view){
+        //listDatos.remove(recycler.getChildAdapterPosition(view));
+        //adapter.notifyItemRemoved(recycler.getChildAdapterPosition(view));
+        listDatos.remove(recycler.getChildAdapterPosition(view));
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            adapter.notifyItemRemoved(recycler.getChildAdapterPosition(view));
+            adapter.notifyItemRangeChanged(recycler.getChildAdapterPosition(view), listDatos.size());
+            swipeRefreshLayout.setRefreshing(false);
+        });
+        int i = recycler.getChildAdapterPosition(view);
+        Toast.makeText(getApplicationContext(), "Eliminat: " + listDatos.get(i) + " refresca la pagina", Toast.LENGTH_SHORT).show();
     }
 }
